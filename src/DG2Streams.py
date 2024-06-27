d82605b385d6510a70b620b9cd20f1fc84bbd088
John Diaz <eljonjairo@gmail.com>
2024-06-27 16:13:36 -0500

    Convert synthetic DG velocity seismograms to obspy streams

---
+++ src/DG2Streams.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# Generate Obspy Stream per Station with Seismic Data
+#
+# John Diaz December 2022
+
+
+import numpy as np
+from os import scandir
+from pathlib import Path
+import obspy
+from obspy.core import UTCDateTime
+from obspy.core.stream import Stream
+import pickle
+
+# DG folder
+DGFolder = "../DGrun_0.5Hz/"
+
+# Output Folder
+SynFolder = "Outputs/SyntheticVelo/"
+# Event time
+teven = "2009-04-06T01:32:40"
+
+# Stations name in the same order as GEODG3D.acquisition DG file.
+Stats = ["AQK", "AQU", "AQV", "AQA", "AQG", "GSA", "MTR", "FMG", "ANT", "AVZ",
+         "CSO1", "LSS", "SUL"]
+
+nstats = int(len(Stats))
+nt = 816
+dt = 0.0489741
+
+print("  ")
+print(" START PROGRAM ")
+print("  ")
+
+subfolders = [f.path for f in scandir(DGFolder) if f.is_dir()]
+for sub_f in subfolders:
+    file = Path(sub_f)
+    file_vx = file / "VX_1"
+    if file_vx.exists():
+        file_vy = file / "VY_1"
+        file_vz = file / "VZ_1"
+        print("Loading DG velocity synthetic files:")
+        print(f"  {file_vx}")
+        print(f"  {file_vy}")
+        print(f"  {file_vz}")
+
+        velo_syn_x = np.reshape(np.fromfile(file_vx, dtype=np.float32),
+                                (nstats, nt), order='F')
+        velo_syn_y = np.reshape(np.fromfile(file_vy, dtype=np.float32),
+                                (nstats, nt), order='F')
+        velo_syn_z = np.reshape(np.fromfile(file_vz, dtype=np.float32),
+                                (nstats, nt), order='F')
+        for istat in range(0, nstats):
+            # DG Data Processing
+            velox_DG = velo_syn_x[istat, :]
+            veloy_DG = velo_syn_y[istat, :]
+            veloz_DG = velo_syn_z[istat, :]
+
+            trace_vx = obspy.Trace()
+            trace_vx.stats.station = Stats[istat]
+            trace_vx.stats.starttime = UTCDateTime(teven)
+            trace_vx.stats.delta = dt
+            trace_vx.stats.npts = nt
+            trace_vx.stats.channel = "DGx"
+            trace_vx.data = velox_DG * 100  # cm/s to m/s
+            # trace_vx.plot()
+
+            trace_vy = obspy.Trace()
+            trace_vy.stats.station = Stats[istat]
+            trace_vy.stats.starttime = UTCDateTime(teven)
+            trace_vy.stats.delta = dt
+            trace_vy.stats.npts = nt
+            trace_vy.stats.channel = "DGy"
+            trace_vy.data = veloy_DG * 100  # cm/s to m/s
+            # trace_vy.plot()
+
+            trace_vz = obspy.Trace()
+            trace_vz.stats.station = Stats[istat]
+            trace_vz.stats.starttime = UTCDateTime(teven)
+            trace_vz.stats.delta = dt
+            trace_vz.stats.npts = nt
+            trace_vz.stats.channel = "DGz"
+            trace_vz.data = veloz_DG * 100  # cm/s to m/s
+            # trace_vz.plot()
+
+            st = Stream([trace_vx, trace_vy, trace_vz])
+            st_file = SynFolder + (sub_f.split('/')[-1] + '_DGVEL_' + Stats[istat]
+                                   + '.pickle')
+            print(f" Writing Stream in file: {st_file} ")
+            st.write(st_file, format="PICKLE")
+
+    else:
+        print(f" file {file_vx} not found")
+
+
+
+
+#     stfile = DGFolder + Stats[istat] + "_DGVEL.pickle"
+#     print(f" Writing Stream in file: {stfile} ")
+#     st.write(stfile, format="PICKLE")
